7aaaa02f17b6945707f72fa6f7ea4183c5c863d5
root <root@NEKONET-AR>
2024-05-30 15:37:54 -0300

    update

--- src/api/app.ts
+++ src/api/app.ts
@@ -86,10 +86,19 @@ export class App {
 
   listenProd(): void {
     var options = {
-      key: fs.readFileSync(path.join('nekoadmin.key'), 'utf8'),
-      cert: fs.readFileSync(path.join('nekoadmin.crt'), 'utf8'),
+      key: fs.readFileSync(
+        path.join('/etc/letsencrypt/live/nekoadmin.com.ar-0002/privkey.pem'),
+        'utf8',
+      ),
+      cert: fs.readFileSync(
+        path.join('/etc/letsencrypt/live/nekoadmin.com.ar-0002/fullchain.pem'),
+        'utf8',
+      ),
     };
-    console.log(' ruta cert', path.join('nekoadmin.crt'));
+    console.log(
+      ' ruta cert',
+      path.join('/etc/letsencrypt/live/nekoadmin.com.ar-0002/privkey.pem'),
+    );
     https.createServer(options, this.app).listen(this.app.get('port'), () => {
       console.log(`Conectado al puerto ${this.app.get('port')}`);
     });
@@ -105,10 +114,20 @@ export class App {
   }
 
   listenSocketProd(): void {
-    const options = {
-      key: fs.readFileSync(path.join('nekoadmin.key'), 'utf8'),
-      cert: fs.readFileSync(path.join('nekoadmin.crt'), 'utf8'),
+    var options = {
+      key: fs.readFileSync(
+        path.join('/etc/letsencrypt/live/nekoadmin.com.ar-0002/privkey.pem'),
+        'utf8',
+      ),
+      cert: fs.readFileSync(
+        path.join('/etc/letsencrypt/live/nekoadmin.com.ar-0002/fullchain.pem'),
+        'utf8',
+      ),
     };
+    console.log(
+      ' ruta cert',
+      path.join('/etc/letsencrypt/live/nekoadmin.com.ar-0002/privkey.pem'),
+    );
     this.server = https.createServer(options, this.socketApp);
     this.webSocketServer = new WebSocketServer(this.server, decodedToken);
     this.server.listen(this.socketApp.get('port'));
