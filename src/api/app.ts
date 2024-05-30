5288aa47e99b53eaf8fe3514b31c893d417aa68b
root <root@NEKONET-AR>
2024-05-30 13:22:16 -0300

    update

--- src/api/app.ts
+++ src/api/app.ts
@@ -88,8 +88,8 @@ export class App {
 
   listenProd(): void {
     var options = {
-      key: fs.readFileSync(path.join(CERT_PATH, 'nekoadmin.crt'), 'utf8'),
-      cert: fs.readFileSync(path.join(CERT_PATH, 'nekoadmin.key'), 'utf8'),
+      key: fs.readFileSync(path.join('nekoadmin.crt'), 'utf8'),
+      cert: fs.readFileSync(path.join('nekoadmin.key'), 'utf8'),
     };
     https.createServer(options, this.app).listen(this.app.get('port'), () => {
       console.log(`Conectado al puerto ${this.app.get('port')}`);
@@ -107,8 +107,8 @@ export class App {
 
   listenSocketProd(): void {
     const options = {
-      key: fs.readFileSync(path.join(CERT_PATH, 'nekoadmin.crt'), 'utf8'),
-      cert: fs.readFileSync(path.join(CERT_PATH, 'nekoadmin.key'), 'utf8'),
+      key: fs.readFileSync(path.join('nekoadmin.crt'), 'utf8'),
+      cert: fs.readFileSync(path.join('nekoadmin.key'), 'utf8'),
     };
     this.server = https.createServer(options, this.socketApp);
     this.webSocketServer = new WebSocketServer(this.server, decodedToken);
